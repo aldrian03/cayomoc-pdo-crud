161b15a3117da2d4c82b08803f08eeb6332c8a1e
aldrian <cayomocjastin@gmail.com>
2024-04-18 11:32:34 +0800

    product_id added

--- public/delete.php
+++ public/delete.php
@@ -1,23 +1,23 @@
 <?php
 // Process delete operation after confirmation
-if(isset($_POST["id"]) && !empty($_POST["id"])){
+if(isset($_POST["product_id"]) && !empty($_POST["product_id"])){
     // Include config file
-    require_once "config.php";
+    require_once "../db/config.php";
     
     // Prepare a delete statement
-    $sql = "DELETE FROM employees WHERE id = ?";
+    $sql = "DELETE FROM products WHERE product_id = ?";
     
-    if($stmt = $mysqli->prepare($sql)){
+    if($stmt = mysqli_prepare($link, $sql)){
         // Bind variables to the prepared statement as parameters
-        $stmt->bind_param("i", $param_id);
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
         
         // Set parameters
-        $param_id = trim($_POST["id"]);
+        $param_id = trim($_POST["product_id"]);
         
         // Attempt to execute the prepared statement
-        if($stmt->execute()){
+        if(mysqli_stmt_execute($stmt)){
             // Records deleted successfully. Redirect to landing page
-            header("location: index.php");
+            header("location: ../index.php");
             exit();
         } else{
             echo "Oops! Something went wrong. Please try again later.";
@@ -25,13 +25,13 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
     }
      
     // Close statement
-    $stmt->close();
+    mysqli_stmt_close($stmt);
     
     // Close connection
-    $mysqli->close();
+    mysqli_close($link);
 } else{
     // Check existence of id parameter
-    if(empty(trim($_GET["id"]))){
+    if(empty(trim($_GET["product_id"]))){
         // URL doesn't contain id parameter. Redirect to error page
         header("location: error.php");
         exit();
@@ -60,11 +60,11 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
                     <h2 class="mt-5 mb-3">Delete Record</h2>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="alert alert-danger">
-                            <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
+                            <input type="hidden" name="product_id" value="<?php echo trim($_GET["product_id"]); ?>"/>
                             <p>Are you sure you want to delete this employee record?</p>
                             <p>
                                 <input type="submit" value="Yes" class="btn btn-danger">
-                                <a href="index.php" class="btn btn-secondary ml-2">No</a>
+                                <a href="index.php" class="btn btn-secondary">No</a>
                             </p>
                         </div>
                     </form>
