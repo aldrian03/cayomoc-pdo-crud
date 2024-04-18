df87b0d7632047f957ef3a776e53591b37e4ff70
aldrian <cayomocjastin@gmail.com>
2024-04-18 11:42:41 +0800

    small changes

--- public/delete.php
+++ public/delete.php
@@ -5,17 +5,17 @@ if(isset($_POST["product_id"]) && !empty($_POST["product_id"])){
     require_once "../db/config.php";
     
     // Prepare a delete statement
-    $sql = "DELETE FROM products WHERE product_id = ?";
+    $sql = "DELETE FROM products WHERE product_id = :product_id";
     
-    if($stmt = mysqli_prepare($link, $sql)){
+    if($stmt = $pdo->prepare($sql)){
         // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        $stmt->bindParam(":product_id", $param_id);
         
         // Set parameters
         $param_id = trim($_POST["product_id"]);
         
         // Attempt to execute the prepared statement
-        if(mysqli_stmt_execute($stmt)){
+        if($stmt->execute()){
             // Records deleted successfully. Redirect to landing page
             header("location: ../index.php");
             exit();
@@ -25,10 +25,10 @@ if(isset($_POST["product_id"]) && !empty($_POST["product_id"])){
     }
      
     // Close statement
-    mysqli_stmt_close($stmt);
+    unset($stmt);
     
     // Close connection
-    mysqli_close($link);
+    unset($pdo);
 } else{
     // Check existence of id parameter
     if(empty(trim($_GET["product_id"]))){
@@ -64,7 +64,7 @@ if(isset($_POST["product_id"]) && !empty($_POST["product_id"])){
                             <p>Are you sure you want to delete this employee record?</p>
                             <p>
                                 <input type="submit" value="Yes" class="btn btn-danger">
-                                <a href="index.php" class="btn btn-secondary">No</a>
+                                <a href="./index.php" class="btn btn-secondary ml-2">No</a>
                             </p>
                         </div>
                     </form>
